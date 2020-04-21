05a89e4beb6f739c2ff4041cc9981f3543ab12c6
Dave Dietrick <djdietrick@gmail.com>
2020-04-21 15:22:08 -0400

    Created home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="home">
-        About
+        <h1 class="heading-primary">Bullet Journal</h1>
+        <h3 class="heading-tertiary">Manage your tasks and events differently.</h3>
+        <p class="paragraph">Task tracker and calendar system based on the pen and paper method.</p>
+        <p class="paragraph">Rapidly log tasks, events, and notes as they happen and track them at daily, monthly, 
+            and yearly levels.</p>
+        <p class="paragraph">Learn more about the method <a href="https://bulletjournal.com/pages/learn">here</a>.</p>
     </div>
 </template>
 
@@ -12,5 +17,21 @@ export default {
 
 <style lang="scss" scoped>
 @import "../styles/main.scss";
+
+.home {
+    margin-top: 5%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-items: center;
+
+    h3 {
+        margin-bottom: 3rem;
+    }
+    
+    a {
+        color: $color-tertiary-dark;
+    }
+}
 
 </style>
