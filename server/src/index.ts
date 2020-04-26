22f63f8bb8b3a25f1da9714743d173277ed3f01b
Dave Dietrick <djdietrick@gmail.com>
2020-04-26 18:34:48 -0400

    Use http server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import {App} from './app';
+const http = require('http');
 
 let port: Number = parseInt(process.env.PORT) || 3000;
 
 const app = new App();
-app.listen(port);
+let server = http.createServer(app.express);
+server.listen(port);
