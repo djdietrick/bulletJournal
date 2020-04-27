04b74a592020024551045352c967605a30b4de78
Dave Dietrick <djdietrick@gmail.com>
2020-04-26 21:57:34 -0400

    Added fs to options

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,14 @@
 import {App} from './app';
 const http = require('https');
+const fs = require('fs');
 
 let port: Number = parseInt(process.env.PORT) || 3000;
 
 const app = new App();
 
 const options = {
-    key: process.env.KEY_LOCATION,
-    cert: process.env.CERT_LOCATION
+    key: fs.readFileSync(process.env.KEY_LOCATION),
+    cert: fs.readFileSync(process.env.CERT_LOCATION)
 }
 
 let server = http.createServer(options, app.express);
